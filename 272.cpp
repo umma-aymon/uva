2fcd5395a81c6a877ac7cb68a36d4b955df49448
u.aymon <37525442+umma-aymon@users.noreply.github.com>
2019-07-26 11:46:05 +0600

    Add files via upload

---
+++ 272.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int c,i;
+    c=0;
+    while(getline(cin,s))
+    {
+        int len=s.length();
+        char ch= {'"'};
+
+        for(i=0; i<len; i++)
+        {
+            if(s[i]==ch)
+            {
+                c++;
+                if(c%2)
+                    cout<<"``";
+                else
+                    cout<<"''";
+            }
+            else
+                cout<<s[i];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
