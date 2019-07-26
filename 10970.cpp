f312e3f039f1c1bf4ded5e93eb0041e3e8f16dda
u.aymon <37525442+umma-aymon@users.noreply.github.com>
2019-07-26 12:18:33 +0600

    Add files via upload

---
+++ 10970.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n,d;
+    while(scanf("%d%d",&m,&n)==2)
+    {
+        d=m*n-1;
+        printf("%d\n",d);
+    }
+    return 0;
+}
